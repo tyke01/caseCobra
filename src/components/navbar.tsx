9d1d6aec82b1e3e57e0ffac5a1b18b619805aa89
victor Macharia <Techvictor653@gmail.com>
2025-02-10 21:18:11 +0300

    feat: finished the navbar and the authentication setup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
 import MaxWidthWrapper from "./max-width-wrapper";
 import { buttonVariants } from "./ui/button";
-import { ArrowRight } from "lucide-react";
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -28,7 +32,7 @@ const Navbar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href={"/api/auth/logout"}
+                    href={"/dashboard"}
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
@@ -71,6 +75,7 @@ const Navbar = () => {
                 </Link>
 
                 <div className="hidden h-8 w-px bg-zinc-200 sm:block" />
+
                 <Link
                   href={"/configure/upload"}
                   className={buttonVariants({
